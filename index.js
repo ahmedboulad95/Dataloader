2ebe001995680a94052d062e278bf0df2fbb9359
Ahmed Boulad <aboulad@AUTOID.local>
2019-06-12 12:47:55 -0500

    Added query for related object metadata

--- index.js
+++ index.js
@@ -11,19 +11,45 @@ conn.login('aboulad@autoidinc.com', 'Ahmdb_95_partialILZWV1mHuUZU6pv48WjzIQtV', 
 
     console.log('Access Token :: ' + conn.accessToken);
     console.log('Instance URL :: ' + conn.instanceUrl);
-    console.log('User Id :: ' + userInfo.id);
+    console.log('User Id :: ' + userInfo.id); 
 
-    
-    
-
-    conn.describe('Loan__c', function(err, metadata) {
+    conn.sobject('Loan__c').describe(function(err, metadata) {
         if(err) console.log("Error :: " + err);
 
-        /*
-        fs.writeFile('response.json', JSON.stringify(metadata), function(err) {
+        
+        /*fs.writeFile('response.json', JSON.stringify(metadata), function(err) {
             if(err) console.log("Error writing file :: " + err);
+        });*/
+        
+        const omitObjects = ['Group', 'User', 'RecordType'];
+
+        let relObjects = [];
+        let lookupObjects = [];
+        metadata.fields.forEach((element) => {
+            if(element.referenceTo.length > 0) {
+                element.referenceTo.forEach((objectName) => {
+                    console.log(objectName);
+                    if(lookupObjects.indexOf(objectName) === -1 && omitObjects.indexOf(objectName) === -1) {
+                        lookupObjects.push(objectName);
+
+                        conn.sobject(objectName).describe((err, relObjMetadata) => {
+                            if(err) console.log('Error in related object call :: ' + err);
+                            /*let fileName = objectName + '.json';
+                            fs.writeFile(fileName, JSON.stringify(relObjMetadata), (err) => {
+                                if(err) console.log('Error writing file :: ' + err);
+                            });*/
+
+                            relObjects.push(relObjMetadata);
+                        });
+                    }
+                });
+            }
         });
-        */
+
+        console.log('\n');
+        console.log(lookupObjects);
+
+        /*
         let queryString = 'SELECT ';
         let fields = [];
         metadata.fields.forEach(function(element) {
@@ -49,6 +75,6 @@ conn.login('aboulad@autoidinc.com', 'Ahmdb_95_partialILZWV1mHuUZU6pv48WjzIQtV', 
                 console.log("Error :: " + err);
             })
             .run({autoFetch : true, maxFetch : 20});
-
+            */
     });
 });
