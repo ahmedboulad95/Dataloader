c40fc3a399f3050ebf1d0ddda986e5f27fc7322c
Ahmed Boulad <aboulad@AUTOID.local>
2019-06-19 15:33:56 -0500

    Application working end to end!

--- index2.js
+++ index2.js
@@ -16,9 +16,12 @@ let recordObj = {};
 let objMetadata = {};
 let stack = [];
 let continueRecurse = false;
+let idMap = {};
 
 // Log in to Salesforce
 conn.login(process.env.SF_DEV_USER, process.env.SF_DEV_PASS, function (err, userInfo) {
+    if (err) throw err;
+
     let currentObj = process.argv[2];
     let limit = process.argv[3];
 
@@ -37,7 +40,36 @@ conn.login(process.env.SF_DEV_USER, process.env.SF_DEV_PASS, function (err, user
             }
         );
 
+        let counter = 0;
+        let keys = Object.keys(recordObj);
+        for(let i = 0; i < keys.length; i++) {
+            insertRecords(recordObj[keys[i]], keys[i]).then(() => {
+                if(counter === keys.length - 1) {
+                    console.dir(idMap);
 
+                    
+                    updateIds();
+                    console.log(JSON.stringify(recordObj))
+                    let newKeys = Object.keys(recordObj);
+                    counter = 0;
+                    for(let j = 0; j < newKeys.length; j++) {
+                        updateRecords(recordObj[newKeys[j]], newKeys[j]).then(() => {
+                            if(counter === newKeys.length - 1) {
+                                console.log("Operation complete");
+                            }
+                            counter++;
+                        }).catch((err) => {
+                            console.log(err);
+                        });
+                    }
+                }
+                counter++;
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+
+        
     }).catch((err) => {
         console.log(err);
     });
@@ -143,8 +175,6 @@ function getRecordsDFS() {
 
         }).then(() => {
             console.log("Got lookup records");
-
-
 
             // continueRecurse is set to true when new records are added to recordObject
             // If no new records are added, no need to explore those relationships
@@ -376,11 +406,77 @@ function findObjectInList(arr, obj) {
     return false;
 }
 
+function insertBulk(records, objectName) {
+    return new Promise((resolve, reject) => {
+        let job = conn.bulk.createJob(objectName, "insert");
+        let batch = job.createBatch();
+
+        batch.execute(records);
+        batch.on("error", (batchInfo) => {
+            reject(batchInfo);
+        });
+        batch.on("queue", (batchInfo) => {
+            batch.poll(1000, 120000);
+        });
+        batch.on("response", (rets) => {
+            for(let i = 0; i < rets.length; i++) {
+                idMap[records[i].Id] = rets[i].id;
+                recordObj[objectName][i].Id = rets[i].id;
+            }
+            resolve();
+        });
+    });
+}
+
+function updateBulk(records, objectName) {
+    return new Promise((resolve, reject) => {
+        let job = conn.bulk.createJob(objectName, "update");
+        let batch = job.createBatch();
+
+        batch.execute(records);
+        batch.on("error", (batchInfo) => {
+            reject(batchInfo);
+        });
+        batch.on("queue", (batchInfo) => {
+            batch.poll(1000, 120000);
+        });
+        batch.on("response", (rets) => {
+            resolve();
+        });
+    });
+}
+
+function updateIds() {
+    let keys = Object.keys(recordObj);
+    for(let i = 0; i < keys.length; i++) {
+        let currentObj = keys[i];
+        console.log(currentObj);
+        let metadata = objMetadata[currentObj];
+        console.log("After getting metadata");
+        for(let j = 0; j < recordObj[currentObj].length; j++) {
+            console.log("Looping through records");
+            for(let k = 0; k < metadata.fields.length; k++) {
+                console.log("Looping through fields");
+                console.log("Field :: " + metadata.fields[k].name);
+
+                if(metadata.fields[k].referenceTo.length > 0 && idMap[recordObj[currentObj][j][metadata.fields[k].name]]) {
+
+                    recordObj[currentObj][j][metadata.fields[k].name] = idMap[recordObj[currentObj][j][metadata.fields[k].name]];
+                }
+            }
+        }
+    }
+}
+
 function insertRecords(records, objectName) {
     return new Promise((resolve, reject) => {
-        conn.sobject(objectName).create(records, { allowRecursive: true }, (err, ret) => {
+        conn.sobject(objectName).create(records, { allowRecursive: true }, (err, rets) => {
             if (err) reject(err);
-            else resolve(ret);
+            for(let i = 0; i < rets.length; i++) {
+                idMap[records[i].Id] = rets[i].id;
+                recordObj[objectName][i].Id = rets[i].id;
+            }
+            resolve();
         });
     });
 }
@@ -389,7 +485,7 @@ function updateRecords(records, objectName) {
     return new Promise((resolve, reject) => {
         conn.sobject(objectName).update(records, { allowRecursive: true }, (err, ret) => {
             if (err) reject(err);
-            else resolve(ret);
+            else resolve();
         });
     });
 }
